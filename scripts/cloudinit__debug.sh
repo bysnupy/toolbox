314e47facfbac3ccdfd5c986a3947adbe6421275
Daein <bysnupy@hotmail.com>
2017-07-18 12:50:43 +0900

    Cloud-init debugging
    debug cloud-init processing without reboot

---
+++ scripts/cloudinit__debug.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#+ Usage: 
+#- debug.sh [--debug]
+
+debug_opt=""
+
+[ "$1" == "--debug" ] && debug_opt="--debug"
+
+# initiate as removed temporary files
+rm -rf /var/lib/cloud/* 
+
+echo "INIT Local phase"
+cloud-init $debug_opt init --local
+
+echo "INIT phase"
+cloud-init $debug_opt init
+
+echo "CONFIG phase"
+cloud-init $debug_opt modules --mode config
+
+echo "FINAL phase"
+cloud-init $debug_opt modules --mode final
+
+exit 0
