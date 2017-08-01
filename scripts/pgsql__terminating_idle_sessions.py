e53ac9642597f68338e9d3c90eb43c6a7c757cde
Daein <bysnupy@hotmail.com>
2017-08-01 11:41:27 +0900

    Update pgsql__terminating_idle_sessions.py

--- scripts/pgsql__terminating_idle_sessions.py
+++ scripts/pgsql__terminating_idle_sessions.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # vi:ft=python
 from subprocess import Popen,PIPE,STDOUT
-import sys,datetime
 
 """
 Purpose: Terminating idle session more than specific times
-Return value:  Terminated session counts
+Return value:  Terminating session counts
 """
 
 # variables
-target_dbname='EC_DB'
-psql_cmd='/usr/local/postgres/bin/psql'
-idle_interval_min='3'
+target_dbname='EC_DB' # target DB
+psql_cmd='/usr/local/postgres/bin/psql' # psql absolute path
+idle_interval_min='10' # idle thresholds
+db_port="5432"         # db_port
 
 extracting_target_sessions_sql='''
 SELECT count(*) FROM (SELECT pg_terminate_backend(pid)
@@ -23,7 +23,7 @@ AND state_change < current_timestamp - INTERVAL '%s' MINUTE) as temp;
 ''' % (target_dbname, idle_interval_min)
 
 # main process
-result = Popen([psql_cmd, '-A', '-t', '-c', extracting_target_sessions_sql], stderr=STDOUT, stdout=PIPE)
+result = Popen([psql_cmd, '-p%s' % db_port, '-A', '-t', '-c', extracting_target_sessions_sql], stderr=STDOUT, stdout=PIPE)
 
 # stdout result
 result_stdout = result.stdout.read()
