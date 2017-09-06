07d8e6c889f98ab345c7e400a64e52f38450e1ad
Daein <bysnupy@hotmail.com>
2017-09-06 16:00:28 +0900

    Update misc__tcp_conn_counts.sh

--- scripts/misc__tcp_conn_counts.sh
+++ scripts/misc__tcp_conn_counts.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
+# Usage: 
+#    arg1: IP:Port
+#    arg2: ss or netstat (default: ss, optional argument)
 #
-# tcp connection counts from ss or netstat output
-# 
+############################################################################
 
-netstat -tna | 
-awk 'BEGIN { printf "%s ", strftime("%H:%M:%S", systime()); conn_counts=0; }
-{ if ( "xxx.xxx.xxx.xxx" == $4 && "ESTABLISHED" == $6 ) conn_counts += 1; }
-END { print conn_counts; }'
+cmdtype="ss"
+if [ $# -gt 1 ]
+then
+  cmdtype=$2
+fi
+target="$1"
+interval=1
+
+echo "Target connection: $target"
+
+while :
+do
+  case $cmdtype in
+    ss) /usr/sbin/ss -tna | awk -v target=$1 '
+       BEGIN { printf "%s ",strftime("%H:%M:%S", systime()); conn_counts=0; }
+       { if ( target == $4 && "ESTAB" == $1 ) conn_counts+=1; }
+       END { print conn_counts; }'
+       sleep $interval
+       ;;
+    netstat) /usr/sbin/netstat -tna | awk -v target=$1 '
+       BEGIN { printf "%s ",strftime("%H:%M:%S", systime()); conn_counts=0; }
+       { if ( target == $4 && "ESTABLISHED" == $6 ) conn_counts+=1; }
+       END { print conn_counts; }'
+       sleep $interval
+       ;;
+    esac
+done
 
 exit $?
